61868f3284a30d1cb4bdf2759c0421daf9c54e20
MaxPyvovarov <max.pivovar@gmail.com>
2024-11-08 13:43:57 +0200

    Update wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -86,7 +86,7 @@ export default {
 	overflow: hidden;
 }
 
-#planning .vuecal__flex.vuecal {
+#planning .vuecal__flex.vuecal[data-v-3b250104] {
 	background: #F3F1ED !important;
 	color: #173F35 !important;
 }
